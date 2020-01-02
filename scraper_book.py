0b37c7c543677e855d68d1d76a1d21cbeff5dbfa
piyushmarya <32293043+piyushmarya@users.noreply.github.com>
2020-01-02 19:03:20 +0530

    Update scraper_book.py

--- scraper_book.py
+++ scraper_book.py
@@ -1,16 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
-URL = "http://books.toscrape.com/"
 
-html_content = requests.get(URL).content
+
 #print(html_content)
 BOOK_LOCATOR = "li.col-xs-6"
 TITLE_LOCATOR = "h3 a"
 RATING_LOCATOR = "p.star-rating"
 PRICE_LOCATOR = "p.price_color"
 LINK_LOCATOR = "h3 a"
-parser = BeautifulSoup(html_content,'html.parser')
+title =[]
+rating = []
+price = []
+link = []
+
+def get_number_pages():
+    URL = "http://books.toscrape.com/catalogue/page-1.html"
+    html_content = requests.get(URL).content
+    parser = BeautifulSoup(html_content,'html.parser')
+    #print((parser.select_one("li.current").string).split("Page"))[3]
+    return 50
 
 
 def find_price(a):
@@ -36,12 +46,18 @@ def find_link(a):
     return link
 
 if __name__ == "__main__":
-    a = parser.select(BOOK_LOCATOR)
-    for i in a:
-        print(find_price(i) + " " +
-        find_rating(i) + " " +
-        find_title(i) + " " +
-        find_link(i)) 
+    for i in range(int(get_number_pages())):
+        URL = "http://books.toscrape.com/catalogue/page-"+str(i)+".html"
+        html_content = requests.get(URL).content
+        parser = BeautifulSoup(html_content,'html.parser')
+        a = parser.select(BOOK_LOCATOR)
+        for i in a:
+            title.append(find_title(i))
+            rating.append(find_rating(i))
+            price.append(find_price(i))
+            link.append(find_link(i)) 
+    mainlist=pd.DataFrame({'Title':title,'Rating':rating,'Price':price,'Link':link})
+    print(mainlist)
 
 
 
