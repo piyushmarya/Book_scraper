b0276a9137ac7f68af4d22e4c5eeb173117ce142
piyushmarya <32293043+piyushmarya@users.noreply.github.com>
2020-01-06 15:25:41 +0530

    added comments

--- scraper_book.py
+++ scraper_book.py
@@ -1,63 +1,92 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
+import re
 
-
-
-#print(html_content)
+# html tag locators to find the values inside the specified tags
 BOOK_LOCATOR = "li.col-xs-6"
 TITLE_LOCATOR = "h3 a"
 RATING_LOCATOR = "p.star-rating"
 PRICE_LOCATOR = "p.price_color"
 LINK_LOCATOR = "h3 a"
-title =[]
+PAGE_LOCATOR = "li.current"
+
+# list initialization to store the values found
+title = []
 rating = []
 price = []
 link = []
 
+
 def get_number_pages():
-    URL = "http://books.toscrape.com/catalogue/page-1.html"
-    html_content = requests.get(URL).content
-    parser = BeautifulSoup(html_content,'html.parser')
-    #print((parser.select_one("li.current").string).split("Page"))[3]
-    return 50
+    """
+    Returns the total number of pages on the specified website
+    """
+    url = "http://books.toscrape.com/catalogue/page-1.html"
+    html_content = requests.get(url).content
+    parser = BeautifulSoup(html_content, "html.parser")
+    content = parser.select_one(PAGE_LOCATOR).string
+    pattern = "Page [0-9]+ of ([0-9]+)"
+    matcher = re.search(pattern, content)
+    pages = int(matcher.group(1))
+    return pages
 
 
-def find_price(a):
-    price = a.select_one(PRICE_LOCATOR)
-    return price.string
-    
+def find_price(book_data):
+    """
+    Returns the price of the specified book
+    """
+    price_of_book = book_data.select_one(PRICE_LOCATOR)
+    return price_of_book.string
 
-def find_rating(a):
-    rating_data = a.select_one(RATING_LOCATOR)
-    rating = rating_data.attrs.get('class')
-    return ([i for i in rating if i not in "star-rating"])[0]
-    
-#    print(rating)
 
-def find_title(a):
-    title_data = a.select_one(TITLE_LOCATOR)
-    title = title_data.attrs['title']
-    return title
+def find_rating(book_data):
+    """
+    Returns the rating of the specified book
+    """
+    rating_data = book_data.select_one(RATING_LOCATOR)
+    rating_of_book = rating_data.attrs.get("class")
+    return ([i for i in rating_of_book if i not in "star-rating"])[0]
 
-def find_link(a):
-    link_data = a.select_one(LINK_LOCATOR)
-    link = link_data.attrs['href']
-    return link
+
+def find_title(book_data):
+    """
+    Returns the title of the specified book
+    """
+    title_data = book_data.select_one(TITLE_LOCATOR)
+    title_of_book = title_data.attrs["title"]
+    return title_of_book
+
+
+def find_link(book_data):
+    """
+    Returns the link of the specified book
+    """
+    link_data = book_data.select_one(LINK_LOCATOR)
+    link_to_book = link_data.attrs["href"]
+    return link_to_book
+
 
 if __name__ == "__main__":
-    for i in range(int(get_number_pages())):
-        URL = "http://books.toscrape.com/catalogue/page-"+str(i)+".html"
+    for page in range(1, get_number_pages() + 1):
+        URL = "http://books.toscrape.com/catalogue/page-" + str(page) + ".html"
+
+        # gathering the html content in var html_content for each page
         html_content = requests.get(URL).content
-        parser = BeautifulSoup(html_content,'html.parser')
-        a = parser.select(BOOK_LOCATOR)
-        for i in a:
-            title.append(find_title(i))
-            rating.append(find_rating(i))
-            price.append(find_price(i))
-            link.append(find_link(i)) 
-    mainlist=pd.DataFrame({'Title':title,'Rating':rating,'Price':price,'Link':link})
-    print(mainlist)
+        parser = BeautifulSoup(html_content, "html.parser")
 
+        # var book_data consists of all the books on the i'th page
+        all_book_data = parser.select(BOOK_LOCATOR)
 
+        # iterating over every book and finding title,rating,price,link
+        for book in all_book_data:
+            title.append(find_title(book))
+            rating.append(find_rating(book))
+            price.append(find_price(book))
+            link.append(find_link(book))
 
+    # creating a dataframe of all the data scraped from the website
+    main_list = pd.DataFrame(
+        {"Title": title, "Rating": rating, "Price": price, "Link": link}
+    )
+    print(main_list)
